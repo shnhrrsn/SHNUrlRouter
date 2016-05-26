e1d3dd7d6b85ffcc42198ba4bdfa28ca51e4d5bc
Alex Manarpies <alex@jarroo.com>
2016-05-26 10:54:47 +0200

    Add project and shared scheme for Carthage support

---
+++ SHNUrlRouter/SHNUrlRouter/SHNUrlRouter.h
@@ -0,0 +1,19 @@
+//
+//  SHNUrlRouter.h
+//  SHNUrlRouter
+//
+//  Created by Alex on 26/05/16.
+//  Copyright © 2016 Shaun Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHNUrlRouter.
+FOUNDATION_EXPORT double SHNUrlRouterVersionNumber;
+
+//! Project version string for SHNUrlRouter.
+FOUNDATION_EXPORT const unsigned char SHNUrlRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHNUrlRouter/PublicHeader.h>
+
+
