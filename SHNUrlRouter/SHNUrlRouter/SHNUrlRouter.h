b7fbc6b6073742640d167353e0bb621cbaa38103
shnhrrsn <shnhrrsn@gmail.com>
2017-10-14 18:11:41 -0400

    Added tvOS framework

--- SHNUrlRouter/SHNUrlRouter/SHNUrlRouter.h
+++ SHNUrlRouter/SHNUrlRouter/SHNUrlRouter.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double SHNUrlRouterVersionNumber;
 FOUNDATION_EXPORT const unsigned char SHNUrlRouterVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SHNUrlRouter/PublicHeader.h>
-
-
